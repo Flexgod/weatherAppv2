141900c87d39bfa666d382d60f315244d205c7c8
akinmaurice <akintayoy@gmail.com>
2017-10-05 12:53:15 +0100

    first commit working template and data

--- routes/index.js
+++ routes/index.js
@@ -5,35 +5,15 @@ let urlreq = 'https://api.darksky.net/forecast/65a5831e7994574a932d16fe38636707/
 
 /* GET home page. */
 router.get('/', function (req, response, next) {
-  https.get(urlreq, function (res) {
-    var info = '';
-    res.on("data", function (chunk) {
-      info += chunk;
-    });
-    res.on("end", function () {
-      if (res.statusCode === 200) {
-        try {
-          const data = JSON.parse(info);
-          //Display Weather for Lagos Nigeria
-          //Convert from Farenheit to Celcius
-          var tempInCelcius = ((data.currently.apparentTemperature - 32) * (5 / 9));
-          tempInCelcius = Math.round( tempInCelcius);
-          var windSpeed = data.currently.windSpeed;
-          var humidity = ((data.currently.humidity) * 100);
-          var icon = data.currently.summary;
-          response.render('index', {Temperature: tempInCelcius, Currently: icon});
-        } catch (error) {
-          console.log("The following error(s) occrured: " + error);
-        }
-      } else {
-        console.log("Something went wrong!");
-        response.redirect('/error');
-      }
-    });
-  });
+  response.render('index', {Temperature: '2', Currently: 'icon'});
 });
 router.get('/error', function(req, res){
   res.render('error');
+})
+
+router.post('/', function(req, response, next){
+  let cityWeather = req.body.city;
+  response.render('weather', { city: cityWeather });
 })
 
 module.exports = router;
