14494173820fb64092e49276b4252bd8f913bb9b
akinmaurice <akintayoy@gmail.com>
2017-10-10 16:01:21 +0100

    updated env variables

---
+++ controller/weather.js
@@ -0,0 +1,3 @@
+//Function to handle the city logitude and latitude
+// Places api key google: AIzaSyA0wJ0mqXhxphr2e14Qo_EZwicKIE-gTVQ
+//function to handle the logitude and latitude weather using darksky api.
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
-let urlreq = 'https://api.darksky.net/forecast/65a5831e7994574a932d16fe38636707/6.5244,3.3792';
+let api_key = process.env.DARK_SKY_API_KEY
+let urlreq = 'https://api.darksky.net/forecast/'+api_key+'/6.5244,3.3792';
 
 /* GET home page. */
 router.get('/', function (req, response, next) {
   response.render('index', {Temperature: '2', Currently: 'icon'});
+  console.log('port value is: '+ process.env);
 });
+/* Route to Get Error page */
 router.get('/error', function(req, res){
   res.render('error');
-})
-
+});
+/* Route handler to seach for city logitude and latitude */
 router.post('/', function(req, response, next){
   let cityWeather = req.body.city;
   response.render('weather', { city: cityWeather });
-})
+});
 
 module.exports = router;
