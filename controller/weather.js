9dd06ec41ff90bc7a84dfa25babbcc07785ad19e
akinmaurice <akintayoy@gmail.com>
2017-10-11 22:09:41 +0100

    fixed temp conversion function

--- controller/weather.js
+++ controller/weather.js
@@ -4,22 +4,20 @@ let apiKey = process.env.DARK_SKY_API_KEY;
 let apiUrl = 'https://api.darksky.net/forecast/';
 let googleKey = process.env.GOOGLE_API_KEY;
 // =================================================
-// Temperature Converter
-// =================================================
-// convert degrees in fahrenheit to celsius
-function fToC(fahrenheit) {
-    var fTemp = fahrenheit,
-        fToCel = (fTemp - 32) * 5 / 9;
-    return fToCel;
-}
-// =================================================
-//Geolocation Detector
+//Geolocation Detector from city name to Geo code
 // =================================================
 function cityGeolocation(city) {
     /*Call google Places API here
     get the Location of the place and call the weather reporter function
     */
-    let googleUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=' + googleKey;
+    let googleApiCall = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=' + googleKey;
+    var request = https.request(googleApiCall, function (err, response, body) {
+        if (!err && response.statusCode === 200) {
+            console.log(body);
+        } else {
+            console.log(err);
+        }
+    });
 }
 // =================================================
 //Weather Reporter
@@ -28,22 +26,28 @@ function weatherReporter(lati, longi) {
     let api_call = apiUrl + apiKey + "/" + lati + "," + longi;
     //Call DarkSkyApi to report weather for location
     var body = "";
-
     var req = https.request(api_call, (res) => {
         res.on('data', (d) => {
             body += d;
         });
-
         res.on('end', () => {
-            var data = body;
-            console.log(data);
+            var data = JSON.parse(body);
+            // =================================================
+            // Temperature Converter from fahrenheit to celcius
+            // =================================================
+            var tempInCelcius = ((data.currently.apparentTemperature - 32) * (5 / 9));
+            tempInCelcius = Math.round(tempInCelcius);
+            var windSpeed = data.currently.windSpeed;
+            var humidity = ((data.currently.humidity) * 100);
+            var icon = data.currently.summary;
+            let list = { tempInCelcius, windSpeed, humidity, icon};
+            console.log(humidity);
         });
     });
 
     req.on('error', (e) => {
         console.error(e);
     });
-
     req.end();
 }
 
