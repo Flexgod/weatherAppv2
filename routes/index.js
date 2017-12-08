cb60890343becc16b9213a801009a2cda6242319
akinmaurice <akintayoy@gmail.com>
2017-12-08 18:45:50 +0100

    bug fixes

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,6 @@ router.get('/error', function (req, res) {
 /* Route handler to seach for city logitude and latitude */
 router.post('/', weatherController.searchCityWeather);
 
-router.post('/api/', weatherController.searchCityWeatherAPI);
+router.get('/api/:lat/:lng', weatherController.searchCityWeatherAPI);
 
 module.exports = router;
